c2ca74fd1481b5f091e3e902c51b9432d45b9826
Catarina Rodrigues <acrm62@gmail.com>
2021-08-14 18:49:17 +0100

    Adding osc messaging in python

--- first_app.py
+++ first_app.py
@@ -1,30 +1,40 @@
 import streamlit as st
 import text2emotion as te
+from pythonosc.udp_client import SimpleUDPClient
 
+ip = "192.168.0.4"  # update to ip of exhibition wifi
+port = 12345
+client = SimpleUDPClient(ip, port)
 
-st.set_page_config(page_title="Networked Loss",
+st.set_page_config(page_title="Networked Veil",
                    layout="centered",
                    initial_sidebar_state="collapsed")
 
 hide_streamlit_style = """
             <style>
-            #MainMenu {visibility: hidden;}
+            # MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
-
-st.title('Networked Loss')
+st.caption('Welcome to')
+st.title('Networked Veil')
 
 with st.form(key='textInput', clear_on_submit=True):
     message = st.text_input(label='Write your message')
     submit = st.form_submit_button(label='Send away')
 
+emotion = ''
+
 if submit:
     final_message = f'{message}'
-    print(final_message)
-
     emotion = te.get_emotion(final_message)
     st.write(emotion)
     print(emotion)
+    values = list(emotion.values())
+    client.send_message("/happy", values[0])
+    client.send_message("/angry", values[1])
+    client.send_message("/surprise", values[2])
+    client.send_message("/sad", values[3])
+    client.send_message("/fear", values[4])
